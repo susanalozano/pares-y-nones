85192edc8b3fb3d9eea795f114a2da4993a7cdae
susanalozano <sussylozig@gmail.com>
2017-03-09 11:03:09 +0100

    script.js

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-//console.log("vamos a jugar a pares y nones");
-
-//var tiradaJugador1 = prompt("elige un numero del 1 al 5");
-//var tiradaJugador2 = prompt("elige un numero del 1 al 5");
-//var tiradaJugador1 = Number(tiradaJugador1);
-//var tiradaJugador2 = Number(tiradaJugador2);
-
-//var sumaTiradas = tiradaJugador1 + tiradaJugador2;
-//console.log(sumaTiradas);
-
-//if(sumaTiradas%2===0){
-    //console.log("Enhorabuena Jugador 1, Has Ganado!!!!!!");
-    
-//}else{
-    //console.log("Enhorabuena Jugador 2, Has Ganado!!!!!!"); 
-    
-//}
-
-
-
-///////////////////////////////////////////correcion de eljuego////////////////////////////////////////////////////////////////////////
 
 console.log("vamos a jugar a pares y nones");
 var jugador1 = prompt("jugador1 dinos un numero del 1 al 5");
